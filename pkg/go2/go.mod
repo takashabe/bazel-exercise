4dbf86ead4a974d7f083240793c65a39f6b61fc7
takashabe <takashabe.io@gmail.com>
2020-06-09 19:47:56 +0900

    depends lib

--- pkg/go2/go.mod
+++ pkg/go2/go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.6.1
+	github.com/takashabe/bazel-exercise/lib/go v0.0.0-20200609103620-5a28c182695b
 )
--- pkg/go2/main.go
+++ pkg/go2/main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	logger "github.com/takashabe/bazel-exercise/lib/go"
+)
+
 func main() {}
 
 func A() string {
 	return "A"
+}
+
+func B() {
+	logger.Info("hoge")
 }
